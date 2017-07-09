6e110b219f812d8873dcd010227dd7351bdb13d8
Chris Beavers <crbeavers@gmail.com>
2017-07-09 11:45:44 -0500

    Cleaner box score printing

--- library/Baseball/BoxScore/Pitching.hs
+++ library/Baseball/BoxScore/Pitching.hs
@@ -132,15 +132,17 @@ addRunsToPitchers fs batter Play{..} p = foldr (chargePitcherForMovement fs batt
 prettyPrintPitching :: Pitching -> Text
 prettyPrintPitching Pitching{..} = unlines
   [ "-------------------------------------------"
-  , "Pitchers        IP   H   R   ER   W  SO  HR"
+  , "Pitchers        IP   H   R  ER   W   SO  HR"
   , "-------------------------------------------"
   , prettyPrintPitchingLines pitchingStats
-  , "-------------------------------------------"
-  , prettyPrintPitchingLine $ set _pitchingLinePlayerId "Total:  " $ mconcat (InsOrdHashMap.elems pitchingStats)
   ]
 
 prettyPrintPitchingLines :: InsOrdHashMap Text PitchingLine -> Text
-prettyPrintPitchingLines pls = unlines $ map prettyPrintPitchingLine (InsOrdHashMap.elems pls)
+prettyPrintPitchingLines pls = unlines $
+  map prettyPrintPitchingLine (InsOrdHashMap.elems pls) ++
+  [ "-------------------------------------------"
+  , prettyPrintPitchingLine $ set _pitchingLinePlayerId "Total:  " $ mconcat (InsOrdHashMap.elems pls)
+  ]
 
 prettyPrintPitchingLine :: PitchingLine -> Text
 prettyPrintPitchingLine PitchingLine{..} = pitchingLinePlayerId
